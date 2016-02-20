426350ae4d677beafbca5f960d2133643ffcfccd
ClockKnight <scottymcphee@gmail.com>
2016-02-20 16:30:00 -0700

    Added login/signup pages for tutors and parents

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,2 +1,11 @@
 class ParentsController < ApplicationController
+
+  def login
+    render 'login'
+  end
+
+  def signup
+    render 'signup'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   root 'non_user#index'
 
+  # route for testing pages that are not accessable
+  # Enter the route to test and go to localhost/test
+  # get '/test', to: 'ENTER ROUTE TO TEST', as: 'test'
+
+  get '/login_parents', to: 'parents#login'
+  get '/login_tutors', to: 'tutors#login'
+  get '/signup_parents', to: 'parents#signup'
+  get '/signup_tutors', to: 'tutors#signup'
+
   resources :tutors, :parents
 end
