b847e1d15a23783b55cc5b5646f1759041d06f09
ClockKnight <scottymcphee@gmail.com>
2016-02-26 14:02:15 -0700

    merged with master fixing excess

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
 class Subject < ActiveRecord::Base
-  
-end
-=======
-class Subject < ActiveRecord::Base
-  belongs_to :tutorialsubject
-  belongs_to :specialization
-  belongs_to :booking
-
-  
 
 end
->>>>>>> master
