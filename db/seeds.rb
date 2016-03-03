ae8e2c72c3b3565b84615792140ad5b1ec4627ba
Ebonkuab <idaku6@yahoo.com>
2016-03-03 07:18:51 +0000

    updating the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Subject.create(subject_name: 'Math')
-Subject.create(subject_name: 'Language Arts')
-Subject.create(subject_name: 'French')
-Subject.create(subject_name: 'Spanish')
-Subject.create(subject_name: 'Social Studies')
-Subject.create(subject_name: 'Science')
-Subject.create(subject_name: 'Chemistry')
-Subject.create(subject_name: 'Physics')
-Subject.create(subject_name: 'Biology')
+
+
+
+
+Specialization.create(years_of_experience: 10, qualification1:'B.Sc Physics', qualification2: 'GED Physics', tutor_detail_id: 12, subject_id: 35 )
+Specialization.create(years_of_experience: 5, qualification1:'B.Sc Mathematics', qualification2: 'Msc Mathematics', tutor_detail_id: 12, subject_id: 28 )
+
+Specialization.create(years_of_experience: 6, qualification1:'B.Sc Mathematics', qualification2: 'Msc Mathematics', tutor_detail_id: 13, subject_id: 28 )
+
+Specialization.create(years_of_experience: 3, qualification1:'B.Sc English', qualification2: 'Diploma English', tutor_detail_id: 14, subject_id: 29 )
+Specialization.create(years_of_experience: 2, qualification1:'B.Sc Spanish', qualification2: '', tutor_detail_id: 14, subject_id: 31 )
+
+Specialization.create(years_of_experience: 8, qualification1:'B.Sc French', qualification2: '', tutor_detail_id: 15, subject_id: 30 )
+Specialization.create(years_of_experience: 5, qualification1:'B.Sc Spanish', qualification2: '', tutor_detail_id: 15, subject_id: 31 )
+
+Specialization.create(years_of_experience: 5, qualification1:'B.Sc Arts', qualification2: '', tutor_detail_id: 16, subject_id: 32 )
+
+Specialization.create(years_of_experience: 12, qualification1:'B.Sc Biology', qualification2: 'Msc Biology', tutor_detail_id: 17, subject_id: 36 )
+
+Specialization.create(years_of_experience: 8, qualification1:'B.Sc Chemistry', qualification2: 'Diploma Chemistry', tutor_detail_id: 18, subject_id: 34 )
+
+Specialization.create(years_of_experience: 5, qualification1:'Diploma Science', qualification2: '', tutor_detail_id: 19, subject_id: 33 )
+
+Specialization.create(years_of_experience: 2, qualification1: 'B.Sc', qualification2: 'B.Ed Minor', tutor_detail_id: 20, subject_id: 36)
+Specialization.create(years_of_experience: 4, qualification1: 'B.Sc', qualification2: 'B.Ed Minor', tutor_detail_id: 20, subject_id: 34)
